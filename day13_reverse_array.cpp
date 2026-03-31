6d1c0c33e795593e18cb21c9ee26e73b3911bf9a
Nishitkumar Patel <161049279+Nikk18@users.noreply.github.com>
2026-03-31 23:37:41 +0530

    Create day13_reverse_array.cpp

---
+++ day13_reverse_array.cpp
@@ -0,0 +1,28 @@
+// Day 13 - Reverse Array
+// Problem: Reverse elements of an array
+// Language: C++
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Enter number of elements: ";
+    cin >> n;
+    int arr[100];
+    cout << "Enter array elements: ";
+    for(int i = 0; i < n; i++)
+        cin >> arr[i];
+
+    for(int i = 0; i < n/2; i++)
+    {
+        int temp = arr[i];
+        arr[i] = arr[n-i-1];
+        arr[n-i-1] = temp;
+    }
+    cout << "Reversed array: ";
+    for(int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+    return 0;
+}
