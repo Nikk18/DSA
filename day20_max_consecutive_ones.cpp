f2c241c7ac76146522042ac0793160a3b8e4f2c2
Nishitkumar Patel <161049279+Nikk18@users.noreply.github.com>
2026-05-01 22:24:24 +0530

    Update day20_max_consecutive_ones.cpp

--- day20_max_consecutive_ones.cpp
+++ day20_max_consecutive_ones.cpp
@@ -10,27 +10,20 @@ int main()
     int n;
     cout << "Enter number of elements: ";
     cin >> n;
-
     int arr[100];
     cout << "Enter array elements (0s and 1s): ";
     for(int i = 0; i < n; i++)
         cin >> arr[i];
-
     int count = 0, maxCount = 0;
-
     for(int i = 0; i < n; i++)
     {
         if(arr[i] == 1)
             count++;
         else
             count = 0;
-
         if(count > maxCount)
             maxCount = count;
     }
-
     cout << "Maximum consecutive ones: " << maxCount;
-
     return 0;
 }
-
